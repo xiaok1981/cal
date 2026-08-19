cd55057a6b5d794f0a293f95641d214fe73a50cb
tony ma <xiaok1981@gmail.com>
2026-08-19 10:05:24 -0700

    add

--- 123.py
+++ 123.py
@@ -1,18 +1,19 @@
-print ('Simple Calculator')
+from operator import add,subtract,multiply,divide
+
+print ('Simple Calculator\n')
 
 num1 = float(input('Enter first number :'))
 num2 = float(input('Enter second number :'))
 
-print ('\nResults:')
+operation = input("Enter operations(+,-,*,/):")
 
-print ('Addition: ', num1 + num2)
-print('subtraction:', num1 - num2)
-print ('multiplication:', num1 * num2)
-
-if num2!=0 :
-    print ('Division:', num1/num2)
+if operation =="+":
+    print(add(num1,num2))
+elif operation =="-":
+    print(subtract(num1,num2))
+elif operation =="*":
+    print(multiply(num1,num2))
+elif operation =="/":
+    print(divide(num1,num2))
 else:
-    print ('Division: Cannot divide by Zero')
-
-
-    
+    print("Invalid Operation")
